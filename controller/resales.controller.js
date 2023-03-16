e46d7b61271250a138e0fb50df31cb5df07a3be5
psukitp <jfellertv@gmail.com>
2023-03-17 02:42:08 +0500

    add detail properties of ad product
    +bug fix

--- controller/resales.controller.js
+++ controller/resales.controller.js
@@ -6,10 +6,27 @@ class ResalesController {
     async createResale(req, res) {
         try {
             const { creator_id, resale_title, resale_description, resale_price, resale_type, resale_tel } = req.body;
-            await db.query(`INSERT INTO ad_post (creator_id, post_name, post_text, price, ad_product_type, ad_telephone) values ($1, $2, $3, $4, $5, $6) RETURNING *`, [creator_id, resale_title, resale_description, resale_price, Number(resale_type), resale_tel])
+            await db.query(`INSERT INTO ad_post (creator_id, post_name, post_text, price, ad_product_type, ad_telephone) values ($1, $2, $3, $4, $5, $6) RETURNING *`, [Number(creator_id), resale_title, resale_description, resale_price, Number(resale_type), resale_tel])
+
 
             const lastValue = await db.query(`SELECT lastval()`)
             const newId = lastValue.rows[0].lastval;
+            switch (Number(resale_type)) {
+                case 1:
+                    await db.query(`INSERT INTO board_property (ad_post_id, board_size, board_deflection, board_flex) values ($1, $2, $3, $4)`, [Number(newId), Number(req.body.length), req.body.deflection, Number(req.body.flex)])
+                    break
+                case 3:
+                    await db.query(`INSERT INTO shoes_property (ad_post_id, shoe_size, shoe_flex) values ($1, $2, $3)`, [Number(newId), req.body.size, Number(req.body.flex)])
+                    break
+                case 4:
+                    await db.query(`INSERT INTO binding_property (ad_post_id, binding_size, binding_flex) values ($1, $2, $3)`, [Number(newId), req.body.size, Number(req.body.flex)])
+                    break
+                case 5:
+                    await db.query(`INSERT INTO clothes_property (ad_post_id, clothes_name, clothes_size) values ($1, $2, $3)`, [Number(newId), req.body.nameof, req.body.size])
+                    break
+                default:
+                    break
+            }
 
             let filePathToGive = '';
             if (req.files !== null) {
@@ -32,14 +49,37 @@ class ResalesController {
 
     async getResales(req, res) {
         try {
-            const events = await db.query(`SELECT ad_post_id, creator_id, post_name, post_text, ad_image_path, price, ad_product_type, ad_telephone, product_type_name FROM
+            let resales = await db.query(`SELECT ad_post_id, creator_id, post_name, post_text, ad_image_path, price, ad_product_type, ad_telephone, product_type_name FROM
             (SELECT * FROM ad_post t1 
             INNER JOIN product_type t2 ON t1.ad_product_type = t2.product_type_id ) AS newtable`)
             res.header({
                 'Access-Control-Allow-Origin': '*'
             });
 
-            res.send(events.rows);
+            let boardProp = await db.query(`SELECT * FROM board_property`);
+            let bindingProp = await db.query(`SELECT * FROM binding_property`);
+            let clothesProp = await db.query(`SELECT * FROM clothes_property`);
+            let shoesProp = await db.query(`SELECT * FROM shoes_property`);
+
+            for (let i = 0; i < resales.rowCount; i++) {
+                let res = boardProp.rows.find(({ ad_post_id }) => ad_post_id === resales.rows[i].ad_post_id);
+                if (res !== undefined) {
+                    Object.assign(resales.rows[i], res)
+                }
+                res = bindingProp.rows.find(({ ad_post_id }) => ad_post_id === resales.rows[i].ad_post_id);
+                if (res !== undefined) {
+                    Object.assign(resales.rows[i], res)
+                }
+                res = clothesProp.rows.find(({ ad_post_id }) => ad_post_id === resales.rows[i].ad_post_id);
+                if (res !== undefined) {
+                    Object.assign(resales.rows[i], res)
+                }
+                res = shoesProp.rows.find(({ ad_post_id }) => ad_post_id === resales.rows[i].ad_post_id);
+                if (res !== undefined) {
+                    Object.assign(resales.rows[i], res)
+                }
+            }
+            res.send(resales.rows);
         } catch (e) {
             console.log(e)
         }
