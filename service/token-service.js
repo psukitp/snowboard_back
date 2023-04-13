c62f7bffa8d75cbe642df97789ce89d63e1fe7f8
psukitp <jfellertv@gmail.com>
2023-04-13 12:13:03 +0500

    delete unnecessary files + token expire 1 day

--- service/token-service.js
+++ service/token-service.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 class TokenService {
     generateTokens(payload) {
-        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '10s' })
+        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '1d' })
         const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: '30d' })
         return {
             accessToken,
